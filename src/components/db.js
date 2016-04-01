6f7d50a070e38bf2a858f54a8eb7bc061f70d230
Peter Tucker <peter.tucker@guaranteedrate.com>
2016-04-01 15:41:18 -0500

    fixing bugs in any db thunk

--- src/components/db.js
+++ src/components/db.js
@@ -1,16 +1,16 @@
 'use strict';
-var db = require('any-db');
 var toolkit = require('../index');
 
 function config(options) {
     var opt = Object.assign({}, options);
+    var db = require('any-db');
     var pool = db.createPool(opt.url, opt.poolConfig);
     var db = {};
     db.execute = function(sql, params) {
         return function(callback) {
             pool.query(sql, params, (error, result) => {
                 if (error) {
-                    toolkit.logger.error('failed to execute sql command: ', { sql: sql, params: params, error: error, connection: settings.database });
+                    toolkit.logger.error('failed to execute sql command: ', { sql: sql, params: params, error: error, connection: opt.url });
                     callback(null, error);
                 } else {
                     callback(null, result.rows);
